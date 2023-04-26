83b14ba278c28c8408710545f1186444e08e8bb9
Guan <821143943@qq.com>
2023-04-26 12:18:05 +0800

    feat: indicate iframe when logging

--- src/client/log.ts
+++ src/client/log.ts
@@ -3,7 +3,27 @@ let log: typeof console.log
 setLogger(console.log)
 
 export function setLogger(logger: typeof console.log) {
-  log = logger.bind(console, `[${GM_info.script.name}]`)
+  let tagName = GM_info.script.name
+
+  try {
+    if (window.top && window.top !== window.self) {
+      let number: number | string = "?"
+
+      if (window.top === window.parent && window.parent.document) {
+        const index = Array.from(window.parent.document?.getElementsByTagName("iframe")).indexOf(
+          window.frameElement as any
+        )
+
+        if (index >= 0) {
+          number = index + 1
+        }
+      }
+
+      tagName = `iframe${number ? `#${number}` : ""}: ${tagName}`
+    }
+  } catch (ignored) {}
+
+  log = logger.bind(console, `[${tagName}]`)
 }
 
 export function tag(text: string, color: string) {
