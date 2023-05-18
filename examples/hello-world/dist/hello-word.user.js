25a8d2aa863499b07863263f971848deb1ff590c
Guan <821143943@qq.com>
2023-05-18 18:50:59 +0800

    chore: add examples/**/dist to git

---
+++ examples/hello-world/dist/hello-word.user.js
@@ -0,0 +1,5 @@
+;(() => {
+  "use strict"
+
+  GM_log("Hello, world!")
+})()
--- examples/hello-world/webpack.config.js
+++ examples/hello-world/webpack.config.js
@@ -1,7 +1,17 @@
 const path = require("path")
 const { monkeyWebpack } = require("webpack-monkey")
 
-module.exports = monkeyWebpack()({
+module.exports = monkeyWebpack({
+  terserPluginOptions: {
+    terserOptions: {
+      compress: {
+        // remove `if (false);` which is converted from `if (module.hot) { ... }`
+        // TODO: remove this when `if (module.hot)` is no longer needed
+        conditionals: true,
+      },
+    },
+  },
+})({
   entry: "./src/index.js",
   output: {
     filename: "hello-word.user.js",
