8f2c5f4cf43d0c48ae9776335b9f8266a4526ae6
Guan <821143943@qq.com>
2023-05-18 18:37:53 +0800

    fix: invalid entry in hello-world example

--- examples/hello-world/webpack.config.js
+++ examples/hello-world/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path")
 const { monkeyWebpack } = require("webpack-monkey")
 
 module.exports = monkeyWebpack()({
-  entry: "src/index.js",
+  entry: "./src/index.js",
   output: {
     filename: "hello-word.user.js",
     path: path.resolve(__dirname, "dist"),
