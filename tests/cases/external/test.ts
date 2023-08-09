8423e9250f468c6053282d8543afeaf2c8b7d4b8
Guan <821143943@qq.com>
2023-08-09 16:10:37 +0800

    test: split case

--- tests/cases/external/test.ts
+++ tests/cases/external/test.ts
@@ -16,7 +16,9 @@ const config = withCommonConfig({
   },
 })
 
-it("build", async () => {
+it("build", () => testBuild(monkeyWebpack()(config)))
+
+it("fails when an unnamed external module is referenced", async () => {
   const faultyConfig = merge({}, config, {
     module: {
       rules: [
@@ -28,9 +30,8 @@ it("build", async () => {
       ],
     },
   })
+
   await expect(testBuild(monkeyWebpack()(faultyConfig))).rejects.toThrowErrorMatchingInlineSnapshot(
     `"Unexpected reference to unnamed external module with URL "https://ex-url". This happens when you import a module from a URL but do not specify an identifier for it, e.g. \`import foo from "https://ex-url"\`, which will cause runtime errors in the generated code. To fix this, either add a universally unique identifier to the import statement, e.g. \`import foo from "foo@https://ex-url"\`, or do not import anything from it, e.g. \`import "https://ex-url"\`."`
   )
-
-  await testBuild(monkeyWebpack()(config))
 })
