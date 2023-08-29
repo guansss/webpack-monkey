45bf6d30ed3b4d1b267b62b6530e427e0a3d9492
Guan <821143943@qq.com>
2023-08-29 18:44:17 +0800

    chore: fix jest error in CI

--- jest.config.js
+++ jest.config.js
@@ -102,7 +102,7 @@ module.exports = {
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-  // modulePathIgnorePatterns: [],
+  modulePathIgnorePatterns: ['dist'],
 
   // Activates notifications for test results
   // notify: false,
